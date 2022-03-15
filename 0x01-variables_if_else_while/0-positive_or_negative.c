0a377c58636fc6d2ab7fc5efe549a65caee8a1dd
Pelumi234 <alakeayobami9@gmail.com>
2022-03-14 20:13:41 -0700

    This source files will print out positive or negative based on entry

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,25 +2,19 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+*main - main block
+*Return: 0
+*/
 int main(void)
 {
 int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-	printf("The number %i is positive\n", n);
-	}
-	else if (n < 0)
-	{
-	printf("The number %i is negative\n", n);
-	}
-	else
-	{
-	printf("The number %i is zero\n", n);
-	}
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+printf("%i is positive\n", n);
+else if (n < 0)
+printf("%i is negative\n", n);
+else
+printf("%i is zero\n", n);
 return (0);
 }
