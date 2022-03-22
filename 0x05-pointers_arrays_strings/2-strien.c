2c74448cada3012811230e668b6139815fc8900e
Pelumi234 <alakeayobami9@gmail.com>
2022-03-21 19:11:35 -0700

    This function returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strien - length of a string
+ * @s: input char
+ * Return: length of a string
+ */
+
+int _strien(char *s)
+{
+	int l = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		l++;
+	}
+	return (1);
+}
