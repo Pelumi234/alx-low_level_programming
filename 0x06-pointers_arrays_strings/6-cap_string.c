c1357bdcb6615cbf94dced708194b45c1dedecd2
Pelumi234 <alakeayobami9@gmail.com>
2022-03-24 06:13:08 -0700

    This capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *cap_string - this is awesome
+ * @s: pointer to char params
+ *
+ * Return: *s
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char delimeters[] = " \t\n,;.!?\"(){}";
+i
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[0] >= 97 && s[0] 	<= 122)
+			s[0] + s[0] - 32;
+				for (j = 0; delimetiers[j] != '\0'; j++)
+					if (s[i + 1] == delimeters[j] && s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
+	}
+	return (s);
+}
