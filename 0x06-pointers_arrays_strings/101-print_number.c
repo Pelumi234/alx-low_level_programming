beef45e7eba475aad9cef1ebc41848fce3d367a9
Pelumi234 <alakeayobami9@gmail.com>
2022-03-24 19:53:25 -0700

    This prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,25 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_number - prints integers
  * @n: number to be printed
- * Return: nothing
  */
 
 void print_number(int n)
 {
-	unsigned int x = n;
+	unsigned int n1;
 
 	if (n < 0)
 	{
+		n1 = -n;
 		_putchar('-');
-		x = -x;
+	} else
+	{
+		n1 = n;
 	}
-	if ((x / 10) > 0)
-		print_number(x / 10);
-	_putchar((x % 10) + '0');
+
+	if (n1 / 10)
+		print_number(n1 / 10);
+
+	_putchar((n1 % 10) + '0');
 }
