d8a05f36df72ddef1ec8e572bbbeef3f8cee3c5e
Pelumi234 <alakeayobami9@gmail.com>
2022-03-28 21:20:02 -0700

    This script contains a function that gets the length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,24 +10,46 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, bool;
+	int i;
+	int j;
+	int c;
 
-	for (j = 0; *(accept + j) != '\0'; j++)
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
 	{
-		bool = 1;
-		for (j = 0; *(accept + j) != '\0'; i++)
+		j = 0;
+		while (accept[j] != '\0')
 		{
-			if (*(s + i) == *(accept + j))
+			if (s[i] == accept[j])
 			{
-				bool = 0;
+				c++;
 				break;
 			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
 	}
-	if (bool == 1)
-		break;
-	}
-	return (i);
+	return (c);
 
 }
-
-
+	/**
+	 * for (j = 0; *(accept + j) != '\0'; j++)
+	 * {
+	 * bool = 1;
+	 * for (j = 0; *(accept + j) != '\0'; i++)
+	 * {
+	 * if (*(s + i) == *(accept + j))
+	 * {
+	 * bool = 0;
+	 * break;
+	 * }
+	 * }
+	 * if (bool == 1)
+	 * break;
+	 * }
+	 * return (i);
+	 */
