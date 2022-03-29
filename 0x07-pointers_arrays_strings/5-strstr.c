297ba93eb11e35eb75ad643d4f4d8c1dd4d21cab
Pelumi234 <alakeayobami9@gmail.com>
2022-03-28 21:53:28 -0700

    This script contains a function that searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,24 +10,17 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, c;
+	int i, j;
 
-	i = 0;
-	c = 0;
-	while (haystack[i] != '\0')
+	for(i = 0; haystack[i] != '\0'; i++)
 	{
-		j = 0;
-		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
-				&& neede[j + c] == haystack[i + c])
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + c] != needle[j + c])
+			if (haystack[i + j] != needle[j])
 				break;
-			c++;
 		}
-		if (needle[j + c] == '\0')
-			return (&haystack[i]);
-		j++;
-		i++;
+		if (!needle[j])
+			return (&haystac[i]);
 	}
 
 	return (NULL);
