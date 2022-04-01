3a2447a3619d574c3064f344d06bf6648d1273e6
Pelumi234 <alakeayobami9@gmail.com>
2022-03-31 20:29:55 -0700

    This function returns 1 if a string is a palindrome and zero if not

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered identical
+ * wildcmp - compares two strings
  * otherwise return 0
  * @s1: input string1
  * @s2: input string2
