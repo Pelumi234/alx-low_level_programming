5cac9c49ce7781d90f7e75e3efcf29172ea37158
Pelumi234 <alakeayobami9@gmail.com>
2022-04-01 08:04:18 -0700

    This prints its name, followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - print the name of the file followed by a new line
@@ -9,7 +10,7 @@
 
 int main(int argc _attribute_((unused)), char *argv[])
 {
-	printf("%s\n", argv[0]);
+	printf("%s\n", *argv);
 
 	return (0);
 }
