331eff0d9d1f0445d610b3d767cf5f868f66be04
Pelumi234 <alakeayobami9@gmail.com>
2022-04-06 20:25:01 -0700

    This creates a static library containing some functions

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of prefix substring
+ * @s: string to be searched
+ * @accept: char array that string will be searched with
+ *
+ * Return: Number of bytes in initial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - a function that locates a substring
+ * @haystack: locate a substring
+ * @needle: string substring to locate
+ * Return: Pointer to beginning of substring, or NULL if string is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *bhaystack;
+	char *pneedle;
+
+	while (*haystack != '\0')
+	{
+		bhaystack = haystack;
+		pneedle = needle;
+
+		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+		{
+			haystack++;
+			pneedle++;
+		}
+		if (!*pneedle)
+			return (bhaystack);
+		haystack = bhaystack + 1;
+	}
+
+	return (0);
+}
